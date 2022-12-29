d26b2e37780b6efe19373678fd8b012162aba9fd
TheFlash566 <mikebabs566@gmail.com>
2022-12-29 07:26:11 -0800

    my first font-awesome usage in react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Font Awesome! <i class="fa-brands fa-font-awesome"></i></h1>
+      <h1>Product Hunt <i class="fa-brands fa-product-hunt"></i></h1>
+      <h1>JavaScript <i class="fa-brands fa-js"></i></h1>
+      <h1>HTML <i class="fa-brands fa-html5"></i></h1>
+      <h1>ReactJs <i class="fa-brands fa-react"></i></h1>
+    </div>
+  )
+}
+
+export default App
